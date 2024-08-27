906b9fd7b8ff844d8a6fb2cf4348e69b6acb83d2
Calmie <safiullinrafa@yandex.ru>
2024-08-28 02:34:21 +0300

    init

---
+++ src/types/components/view/card.ts
@@ -0,0 +1,9 @@
+export interface ICardView {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: string;
+    button: HTMLButtonElement;
+}
