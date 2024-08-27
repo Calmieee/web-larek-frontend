0b9ce82231cf9391c95099e954877e7dd6830c98
Calmie <safiullinrafa@yandex.ru>
2024-08-28 02:04:27 +0300

    fix

--- src/types/components/base/api.ts
+++ src/types/components/base/api.ts
@@ -5,17 +5,38 @@ export type ApiListResponse<Type> = {
 
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
-export interface ErrorState {
-    error: string;
-}
+export class Api {
+    readonly baseUrl: string;
+    protected options: RequestInit;
 
-export interface HandleResponce {
-    handleResponce(response: Response): Promise<object>
-}
+    constructor(baseUrl: string, options: RequestInit = {}) {
+        this.baseUrl = baseUrl;
+        this.options = {
+            headers: {
+                'Content-Type': 'application/json',
+                ...(options.headers as object ?? {})
+            }
+        };
+    }
 
-export interface Api<T extends object> extends HandleResponce {
-    baseUrl: string;
-    headers: T;
-    get(uri: string): HandleResponce;
-    post(uri: string, data: object, method: ApiPostMethods): HandleResponce;
+    protected handleResponse(response: Response): Promise<object> {
+        if (response.ok) return response.json();
+        else return response.json()
+            .then(data => Promise.reject(data.error ?? response.statusText));
+    }
+
+    get(uri: string) {
+        return fetch(this.baseUrl + uri, {
+            ...this.options,
+            method: 'GET'
+        }).then(this.handleResponse);
+    }
+
+    post(uri: string, data: object, method: ApiPostMethods = 'POST') {
+        return fetch(this.baseUrl + uri, {
+            ...this.options,
+            method,
+            body: JSON.stringify(data)
+        }).then(this.handleResponse);
+    }
 }
